694c5338975f0e59c4d8b147327b0b2d44df7af5
Ayush Mishra <ayush110702@gmail.com>
2024-08-26 23:21:00 +0530

    Images/DarkOverlay added

---
+++ Frontened/src/controllers/DarkOverlay.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DarkOverlay() {
+  return <>
+        <div className="absolute inset-0 md:h-screen bg-black opacity-70 z-10 ">
+
+        </div>
+  
+    </>;
+}
+
+export default DarkOverlay;
--- Frontened/src/main.jsx
+++ Frontened/src/main.jsx
@@ -1,24 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import './index.css'
-import { createBrowserRouter, RouterProvider,createRoutesFromElements,Route } from 'react-router-dom'
-import Layout from './Layout.jsx'
-import Header from './controllers/Header.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
-import Contact from './pages/Contact.jsx'
+import "./index.css";
+import {
+	createBrowserRouter,
+	RouterProvider,
+	createRoutesFromElements,
+	Route,
+} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Header from "./controllers/Header.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout/>}>
-      <Route path='' element={<Header/>} />
-      <Route path='home' element={<Home/>} />
-      <Route path='about' element={<About/>} />
-      <Route path='contact' element={<Contact/>} />
-
-    </Route>
-  )
-)
+	createRoutesFromElements(
+		<Route path="/" element={<Layout />}>
+			<Route path="" element={<Header />} />
+			<Route path="home" element={<Home />} />
+			<Route path="about" element={<About />} />
+			<Route path="contact" element={<Contact />} />
+		</Route>
+	)
+);
 /*
 const router = createBrowserRouter([
   {
@@ -33,8 +37,8 @@ const router = createBrowserRouter([
     ]
   }
 ])*/
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
